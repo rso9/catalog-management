15baf5a1e42be3f192a87bc2a8127d3fddebe2d0
Matej Klemen <matej.klemen1337@gmail.com>
2018-10-27 15:39:40 +0200

    Add CRUD operations logic (API + bean) for albums

--- api/src/main/java/rest/v1/sources/AlbumSource.java
+++ api/src/main/java/rest/v1/sources/AlbumSource.java
@@ -15,6 +15,7 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -28,18 +29,42 @@ public class AlbumSource {
     private AlbumBean albumBean;
 
     @Operation(
+            summary = "Get all albums",
+            description = "Get all albums",
+            tags = "album",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Albums",
+                            content = @Content(schema = @Schema(implementation = Album.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No albums in the catalog",
+                            content = @Content(schema = @Schema(implementation = Error.class))
+                    )
+            })
+    @GET
+    public Response getAllAlbums() {
+        List<Album> albums = albumBean.getAlbums();
+
+        return albums == null? Response.status(Response.Status.NOT_FOUND).build():
+                Response.status(Response.Status.OK).entity(albums).build();
+    }
+
+    @Operation(
             summary = "Get an album by ID",
             description = "Get an album by ID",
             tags = "album",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "Artist",
-                            content = @Content(schema = @Schema(implementation = Artist.class))
+                            description = "Album",
+                            content = @Content(schema = @Schema(implementation = Album.class))
                     ),
                     @ApiResponse(
                             responseCode = "404",
-                            description = "No artist with ID in the catalog",
+                            description = "No album with given ID in the catalog",
                             content = @Content(schema = @Schema(implementation = Error.class))
                     )}
     )
@@ -51,22 +76,76 @@ public class AlbumSource {
                 Response.status(Response.Status.OK).entity(album).build();
     }
 
-    /*
-    * TODO:
-    * - decide whether this endpoint even makes sense
-    * (an alternative could be using PUT /album/ with an updated list of artists)
-    * - change what the function returns
-    * - document the responses
     @Operation(
-            description = "Add artist to an existing album",
-            tags = "album")
-    @Path("{id}/addArtist")
+            summary = "Add a new album",
+            description = "Add a new album",
+            tags = "album",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Album",
+                            content = @Content(schema = @Schema(implementation = Album.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Could not add album to catalog",
+                            content = @Content(schema = @Schema(implementation = Error.class))
+                    )}
+    )
     @POST
-    public Response addArtistToAlbum(@PathParam("id") int idAlbum, @RequestBody Artist artist) {
-        boolean successAdd = artistAlbumBean.joinArtistAndAlbum(artist.getId(), idAlbum);
+    public Response addAlbum(@RequestBody Album album) {
+        Album newAlbum = albumBean.addAlbum(album);
 
-        return successAdd? Response.status(Response.Status.OK).build():
+        return newAlbum == null? Response.status(Response.Status.BAD_REQUEST).build():
+                Response.status(Response.Status.OK).entity(newAlbum).build();
+    }
+
+    @Operation(
+            summary = "Update an album entity",
+            description = "Update an album entity. If the album does not exist, it is created. " +
+                    "The operation is idempotent",
+            tags = "album",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Album",
+                            content = @Content(schema = @Schema(implementation = Album.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Updating the album failed",
+                            content = @Content(schema = @Schema(implementation = Error.class))
+                    )}
+    )
+    @PUT
+    public Response addOrUpdateAlbum(@RequestBody Album album) {
+        Album newAlbum = albumBean.updateAlbum(album);
+
+        return newAlbum == null? Response.status(Response.Status.BAD_REQUEST).build():
+                Response.status(Response.Status.OK).entity(newAlbum).build();
+    }
+
+    @Operation(
+            summary = "Delete an album",
+            description = "Delete album by ID",
+            tags = "album",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully removed album"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Removal of album failed",
+                            content = @Content(schema = @Schema(implementation = Error.class))
+                    )}
+    )
+    @Path("{id}")
+    @DELETE
+    public Response deleteAlbum(@PathParam("id") int idAlbum) {
+        boolean status = albumBean.deleteAlbum(idAlbum);
+
+        return status? Response.status(Response.Status.OK).build():
                 Response.status(Response.Status.BAD_REQUEST).build();
     }
-    */
 }
--- services/src/main/java/beans/AlbumBean.java
+++ services/src/main/java/beans/AlbumBean.java
@@ -5,6 +5,9 @@ import core.Album;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @ApplicationScoped
 public class AlbumBean {
@@ -14,5 +17,41 @@ public class AlbumBean {
 
     public Album getAlbumById(int idAlbum) {
         return entityManager.find(Album.class, idAlbum);
+    }
+
+    public List<Album> getAlbums() {
+        TypedQuery<Album> query = entityManager.createNamedQuery("Album.getAll", Album.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Album addAlbum(Album album) {
+        if(album == null)
+            return null;
+
+        entityManager.persist(album);
+        return album;
+    }
+
+    /* Updates the Album instance with same id as `album`.
+        If such an instance does not exist, it is created. */
+    @Transactional
+    public Album updateAlbum(Album album) {
+        if(album == null)
+            return null;
+
+        entityManager.merge(album);
+        return album;
+    }
+
+    @Transactional
+    public boolean deleteAlbum(int idAlbum) {
+        try {
+            Album wantedAlbum = entityManager.find(Album.class, idAlbum);
+            entityManager.remove(wantedAlbum);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
     }
 }
