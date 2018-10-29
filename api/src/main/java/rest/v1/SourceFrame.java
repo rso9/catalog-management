e398efffc070c7cab8eff648637f0f254905b390
Matej Klemen <matej.klemen1337@gmail.com>
2018-10-29 17:22:49 +0100

    Add a few annotations to base of REST API due to 'mvn clean package' throwing errors otherwise

--- api/src/main/java/rest/v1/SourceFrame.java
+++ api/src/main/java/rest/v1/SourceFrame.java
@@ -3,13 +3,17 @@ package rest.v1;
 import javax.ws.rs.ApplicationPath;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import rest.v1.sources.*;
 
 @OpenAPIDefinition(info = @Info(
-        title = "Rest API for catalog management",
+        title = "REST API for catalog management",
         version = "v1",
-        description = "Manage metadata of artists, tracks, ... in the catalog"))
+        description = "Manage metadata of artists, tracks, ... in the catalog",
+        contact = @Contact(name = "Joze", email = "joze@not-a-real-mail.com"),
+        license = @License(name = "")))
 @ApplicationPath("v1")
 public class SourceFrame extends javax.ws.rs.core.Application {
 }
