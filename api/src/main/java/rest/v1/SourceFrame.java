c7391fceb48141e2dfe200487117f071c0f69e3a
Matej Klemen <matej.klemen1337@gmail.com>
2019-01-05 18:45:00 +0100

    Add CORS support

--- api/src/main/java/rest/v1/SourceFrame.java
+++ api/src/main/java/rest/v1/SourceFrame.java
@@ -2,6 +2,7 @@ package rest.v1;
 
 import javax.ws.rs.ApplicationPath;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -17,5 +18,6 @@ import rest.v1.sources.*;
         license = @License(name = "")))
 @ApplicationPath("v1")
 @RegisterService
+@CrossOrigin(supportedMethods = "GET, POST, PUT, DELETE, HEAD, OPTIONS")
 public class SourceFrame extends javax.ws.rs.core.Application {
 }
